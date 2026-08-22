9dc358a1af6588bf2e7ca6959988eec2f6555657
Catlak Profesor <progesor@gmail.com>
2026-08-22 12:03:25 +0300

    feat(m3): register lead CRM repository

--- src-tauri/src/repositories/mod.rs
+++ src-tauri/src/repositories/mod.rs
@@ -1,5 +1,6 @@
 pub mod contact_repository;
 pub mod import_history_repository;
 pub mod import_preview_repository;
+pub mod lead_crm_repository;
 pub mod lead_detail_repository;
 pub mod lead_workspace_repository;

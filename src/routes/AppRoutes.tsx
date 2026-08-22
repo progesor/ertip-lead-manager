205bf89f83dbda952d66c5c829f7a3b781f1ec5e
Catlak Profesor <progesor@gmail.com>
2026-08-22 11:17:27 +0300

    feat(m3): route lead detail workspace

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { AppShell } from "../app/AppShell";
 import { AnalyticsPage } from "../features/analytics/AnalyticsPage";
 import { DashboardPage } from "../features/dashboard/DashboardPage";
 import { ImportsPage } from "../features/imports/ImportsPage";
+import { LeadDetailPage } from "../features/leads/LeadDetailPage";
 import { LeadsPage } from "../features/leads/LeadsPage";
 import { PipelinePage } from "../features/pipeline/PipelinePage";
 import { SettingsPage } from "../features/settings/SettingsPage";
@@ -13,6 +14,7 @@ export function AppRoutes() {
       <Route element={<AppShell />}>
         <Route index element={<DashboardPage />} />
         <Route path="leads" element={<LeadsPage />} />
+        <Route path="leads/:leadId" element={<LeadDetailPage />} />
         <Route path="pipeline" element={<PipelinePage />} />
         <Route path="analytics" element={<AnalyticsPage />} />
         <Route path="imports" element={<ImportsPage />} />
